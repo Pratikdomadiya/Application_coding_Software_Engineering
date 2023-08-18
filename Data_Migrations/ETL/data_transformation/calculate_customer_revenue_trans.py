c02b8d2f2e4a371a1482f7f64171490ec952d6a2
Pratikdomadiya <pratikdomadiya123@gmail.com>
2023-08-17 22:43:07 -0400

    Adding files and packages

--- Data_Migrations/ETL/data_transformation/calculate_customer_revenue_trans.py
+++ Data_Migrations/ETL/data_transformation/calculate_customer_revenue_trans.py
@@ -2,7 +2,19 @@
 
 # Your transformation logic for data set 2
 import pandas as pd
+
+
 def calculate_customer_revenue(customers_file, orders_file):
+    """
+    Calculate total revenue for each customer based on customer and order data.
+
+    Args:
+        customers_file (str): Path to the CSV file containing customer data.
+        orders_file (str): Path to the CSV file containing order data.
+
+    Returns:
+        pandas.DataFrame: DataFrame with customer IDs and their total revenue.
+    """
     customers = pd.read_csv(customers_file)
     orders = pd.read_csv(orders_file)
 
@@ -11,6 +23,7 @@ def calculate_customer_revenue(customers_file, orders_file):
 
     customer_revenue = merged_data.groupby('customer_id')['revenue'].sum().reset_index()
     return customer_revenue
+
 
 customers_data = 'customers.csv'
 orders_data = 'orders.csv'
--- Data_Migrations/ETL/data_transformation/calculate_total_sales_trans.py
+++ Data_Migrations/ETL/data_transformation/calculate_total_sales_trans.py
@@ -4,8 +4,19 @@ import pandas as pd
 
 
 # Your transformation logic for data set 1
-def calculate_total_sales(data):
-    data = pd.read_csv(data)
+import pandas as pd
+
+def calculate_total_sales(input_file):
+    """
+    Calculate total sales for each product.
+
+    Args:
+        input_file (str): Path to the CSV file containing sales data.
+
+    Returns:
+        pandas.DataFrame: DataFrame with product names and their total sales.
+    """
+    data = pd.read_csv(input_file)
     product_sales = data.groupby('product')['sales'].sum().reset_index()
     return product_sales
 
