ad3a9c966b0a5feb36d3f346fac8ad0c6662cb06
Pratikdomadiya <pratikdomadiya123@gmail.com>
2023-10-14 21:51:02 -0400

    design patterns continue

--- SDE - Advance Coding /SDE - Design patterns /Creational Design Patterns/singleton/sample_code.py
+++ SDE - Advance Coding /SDE - Design patterns /Creational Design Patterns/singleton/sample_code.py
@@ -1,24 +1,30 @@
 class Singleton:
     __instance = None
 
-    def __init__(self):
+    def __init__(self, data=None):
       """ Virtually private constructor. """
       if Singleton.__instance != None:
-         raise Exception("This class is a singleton!")
+          raise Exception("This class is a singleton!")
       else:
-         Singleton.__instance = self
+        Singleton.__instance = self
+        Singleton.__instance.data = data
+        print("data is set")
+
+
 
 
     @staticmethod
     def getInstance():
       """ Static access method. """
       if Singleton.__instance == None:
-         Singleton()
-      return Singleton.__instance
+        Singleton()
+      return (Singleton.__instance.data)
+      # return Singleton.__instance
 
 
 if __name__ == "__main__":
-    s = Singleton()
+    s = Singleton(data= "shared resources")
+
     print (s)
 
     s1 = Singleton.getInstance()
